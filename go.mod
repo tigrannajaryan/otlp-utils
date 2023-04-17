bef5173330f72f9391e364fccff50bc212e35380
Tigran Najaryan <tnajaryan@splunk.com>
2023-04-17 19:16:49 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/tigrannajaryan/otlp_utils
+
+go 1.19
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	go.opentelemetry.io/collector/pdata v0.66.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.51.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
+	"go.opentelemetry.io/collector/pdata/ptrace/ptraceotlp"
+)
+
+var help = flag.Bool("help", false, "Converts traces from OTLP JSON format to OTLP Protobuf format")
+var inputFile = ""
+var outputFile = ""
+
+// This tool generates a trace dataset in the OpenTelemetry Protocol format from a fake traces generator.
+func main() {
+	// Define the flags.
+	flag.StringVar(&inputFile, "input", outputFile, "Input OTLP JSON file")
+	flag.StringVar(&outputFile, "output", outputFile, "Output OTLP Protobuf file")
+
+	// Parse the flag
+	flag.Parse()
+
+	// Usage Demo
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	inputContent, err := os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatal("Cannot read input file: ", err)
+	}
+
+	jsons := strings.Split(string(inputContent), "\n")
+
+	var msg []byte
+	if strings.HasPrefix(string(inputContent), "{\"resourceSpans\":") {
+		combined := ptraceotlp.NewExportRequest()
+		for _, json := range jsons {
+			if json == "" {
+				continue
+			}
+			request := ptraceotlp.NewExportRequest()
+			err = request.UnmarshalJSON([]byte(json))
+			if err != nil {
+				log.Fatal("Unmarshalling error: ", err)
+			}
+			request.Traces().ResourceSpans().MoveAndAppendTo(combined.Traces().ResourceSpans())
+		}
+
+		// Marshal the request to bytes.
+		msg, err = combined.MarshalProto()
+		if err != nil {
+			log.Fatal("Marshaling error: ", err)
+		}
+	} else if strings.HasPrefix(string(inputContent), "{\"resourceMetrics\":") {
+		combined := pmetricotlp.NewExportRequest()
+		for _, json := range jsons {
+			if json == "" {
+				continue
+			}
+			request := pmetricotlp.NewExportRequest()
+			err = request.UnmarshalJSON([]byte(json))
+			if err != nil {
+				log.Fatal("Unmarshalling error: ", err)
+			}
+			request.Metrics().ResourceMetrics().MoveAndAppendTo(combined.Metrics().ResourceMetrics())
+		}
+
+		// Marshal the request to bytes.
+		msg, err = combined.MarshalProto()
+		if err != nil {
+			log.Fatal("Marshaling error: ", err)
+		}
+	} else {
+		log.Fatalf("Input file format unrecognized\n")
+	}
+
+	// Write protobuf to file
+	if _, err := os.Stat(outputFile); os.IsNotExist(err) {
+		err = os.MkdirAll(path.Dir(outputFile), 0700)
+		if err != nil {
+			log.Fatal("Error creating directory: ", err)
+		}
+	}
+	err = os.WriteFile(outputFile, msg, 0600)
+	if err != nil {
+		log.Fatal("Cannot write file: ", err)
+	}
+}
